d4eaf93cb37d753e11d597d64ccfc6757f04cf8d
Teiko19 <91704073+Teiko19@users.noreply.github.com>
2022-12-25 17:56:35 -0300

    fi components

--- components/Article/Article.tsx
+++ components/Article/Article.tsx
@@ -3,10 +3,8 @@ import styles from './article.module.scss';
 
 export const Article = () => {
   return (
-    <>
-      <article>
-        <h1 className={styles.h1}>Tomas Ariel Belvedere</h1>
-      </article>
-    </>
+    <article>
+      <h1 className={styles.h1}>Tomas Ariel Belvedere</h1>
+    </article>
   )
 };
